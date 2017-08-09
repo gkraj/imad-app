7402a0e092031d22d1d93220896b0207b5a1bc14
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-09 16:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res){
-    res.send('Article one Resuested will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article_two',function (req,res){
     res.sendFile('Article two requested will be served here');
