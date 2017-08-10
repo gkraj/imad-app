08c79d929eef15c03ca5c705f26f3516edfba612
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-10 11:59:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTMl  = "New Value";
+var element = document.getElementById('main-text');
+element.innerHTMl  = 'New Value';
