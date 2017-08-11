678554d388564114d08d9ff36b28502dc08355eb
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-11 12:21:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/:counter', function(req,res){
+app.get('/counter', function(req,res){
     counter = counter+1;
    res.send(counter.toString()); 
 });
