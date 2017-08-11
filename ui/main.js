7e2f97aadeaae246bca541448e789a4ad5594588
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-11 12:38:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML  = 'New Value';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-  marginLeft = marginLeft + 5;
-  img.style.marginLeft = marginLeft+'px';
-}
-img.onclick = function(){
-  var interval = setInterval(moveRight,50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+  var span = document.getElementById('count');
+  count = counter +1;
+  span.innerHTML = counter.toString(); 
+    
 };
