e4ce1b659045571b5675348cc6b8ead701de1730
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-13 01:34:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,7 @@ button.onclick = function(){
   request.open('GET', 'http://ggokulrajan.imad.hasura-app.io/counter', true);
   request.send(null);
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
@@ -40,6 +39,8 @@ submit.onclick = function(){
     //not ready
   };
   //made a request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value
   request.open('GET', 'http://ggokulrajan.imad.hasura-app.io/submit-name?name='+name, true);
   request.send(null);
 };
