acffc09e2ccc476dce9171905b7fe0a75d802fab
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-12 23:34:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,11 +22,11 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
-    var names = ['name1', 'name2', 'name3'];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for(i=0;name++;i<names.length){
+    for( var i=0; name++; i<names.length){
         list += '<li>'+names[i]+ '</li>';
     }
-  var ul = document.getElementById('namelist');
+  var ul = document.getElementById('nameList');
   ul.innerHTML = list;
 };
