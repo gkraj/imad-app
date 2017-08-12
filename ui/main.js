8b00b991d5b675b0cf64639c64060052d9cf8d25
Gokul Kannan <ggokulrajan@gmail.com>
2017-08-12 16:00:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
-  var span = document.getElementById('count');
-  counter = counter +1;
-  span.innerHTML = counter.toString(); 
-    
+//crate a request
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      //if ready
+    if (request.readystate === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var counter = request.responsetext;
+            var span = document.getElementbyId('count');
+            span.innerHTML = counter.toString();
+        }
+    }  
+    //not ready
+  };
+  //made a request
+  request.open('GET', 'https://http://ggokulrajan.imad.hasura-app.io/counter', true);
+  request.send('null');
+
 };
