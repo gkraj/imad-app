cbf198c55ec0f1b3c8b834e545b10f7889134f7a
Gokul Kannan <ggokulrajan@gmail.com>
2017-09-22 10:25:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,33 @@ app.post('/create-user', function(req,res){
    });
 });
 
+app.post('/login', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1',[username], function(err, res){
+      if (err){
+          res.status(500).send(err.toString());
+      } else{
+          if(result.rows.length === 0){
+              // if table contain zero rows
+              res.send(403).send('no user name found');
+          }else{
+              //match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt);
+              if(hashedPassword === dbString){
+                  res.send('password is correct');
+              }else{
+                  res.send(403).send('password is incorrect');
+              }
+              
+          } 
+      }
+   });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req,res){
     //make a req
