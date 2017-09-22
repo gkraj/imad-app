5d933bcc0487fbd25933e8d519dfb3f42cce9249
Gokul Kannan <ggokulrajan@gmail.com>
2017-09-22 10:59:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,5 @@
-var button = document.getElementById('counter');
 
-button.onclick = function(){
-//crate a request
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function(){
-      //if ready
-    if (request.readyState === XMLHttpRequest.DONE){
-        if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }  
-    //not ready
-  };
-  //made a request
-  request.open('GET', 'http://ggokulrajan.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
-
+//login form
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
@@ -26,62 +7,21 @@ submit.onclick = function(){
       //if ready
         if (request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for( var i=0;i<names.length; i++ ){
-                list += '<li>'+names[i]+ '</li>';
-                 }
-     var ul = document.getElementById('nameList');
-     ul.innerHTML = list;
-        }
+                alert('successfully logged in');
+                }else if (request.status === 403){
+                    alert('username/password is incorrect');
+                }else if (request.status === 403){
+                    alert('something wrong in server');
+                }
     }  
     //not ready
   };
   //made a request
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET', 'http://ggokulrajan.imad.hasura-app.io/submit-name?name='+name, true);
-  request.send(null);
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST', 'http://ggokulrajan.imad.hasura-app.io/login', true);
+  request.setRequestHeader ('Content-Type', 'application/json');
+  request.send(JSON.stringify({username: username, password: password}));
 };
-
-/*var submitobj = document.getElementById('submit_button'); 
-    //value of id assign it to submitobj
-submit.onclick = function(){ 
-    //clicking the submit btn will call fnt
-    var requestobj = new XMLHttpRequest(); 
-    // create obj requestobj for html getting value as html
-    // after clicking sending hte request to server as html value
-    request.onreadystatechange = function(){
-    if (requestobj.readystate === XMLHttpRequest.DONE){ 
-        if(requestobj.response === 200){ 
-            //after successfully sending res to server it will resposne as 200 means success
-            //creating obj for response text which we entered
-            //json.parse is used to convet the html to js obj
-            var commentobj = JSON.parse (requestobj.responseText);
-            var comment_txt = '';
-            for (i=0; i<comment_txt.length; i++){
-                comment_txt += '<p>' + commentobj[i] +'</p>';
-            }
-            var comment = document.getElementById('comments');
-            comment.innerHTML = comment_txt;
-        }
-    }
-    };
-var text_out = document.getElementById('text_box');
-var text_box = text_out.value;
-request.open('GET', 'http://ggokulrajan.imad.hasura-app.io/articleName/submit?text_box=' +text_box, true);
-request.send(null);
- };
- */
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
