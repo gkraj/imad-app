f538366fb22b4de00bb4b6f51999df44042dcf19
Gokul Kannan <ggokulrajan@gmail.com>
2017-09-26 12:36:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/login', function(req,res){
       if (err){
           res.status(500).send(err.toString());
       } else{
-          if(results.rows.length === 0){
+          if(result.rows.length === 0){
               // if table contain zero rows
               res.send(403).send('no user name found');
           }else{
