558f2213660f4decfc5fa6b8b7383af9b53a477e
Gokul Kannan <ggokulrajan@gmail.com>
2017-09-27 07:54:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,5 +23,5 @@ submit.onclick = function(){
   console.log(password);
   request.open('POST', 'http://ggokulrajan.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify({"username": username, "password": password}));
+  request.send(JSON.stringify({username: username, password: password}));
 };
