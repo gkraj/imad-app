2c2275236c676cdfd6004abd4b6fc774d063e3af
Gokul Kannan <ggokulrajan@gmail.com>
2017-09-28 07:32:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/create-user', function(req,res){
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt); 
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, res){
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result){
       if (err){
           res.status(500).send(err.toString());
       }else{
@@ -98,7 +98,7 @@ app.post('/login', function(req,res){
    var username = req.body.username;
    var password = req.body.password;
    
-   pool.query('SELECT * FROM "user" WHERE username = $1',[username], function(err, res){
+   pool.query('SELECT * FROM "user" WHERE username = $1',[username], function(err, result){
       if (err){
           res.status(500).send(err.toString());
       } else{
